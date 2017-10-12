2641cbb9e849eea3e9f14d253fcd234787eaeb3f
rahulthagadur <rt_rahul@hotmail.com>
2017-10-12 23:06:20 +0530

    Initial commit | | Final Commit

---
+++ app/src/main/java/com/example/thagadur/android_session11_assignment2/MainActivity.java
@@ -0,0 +1,98 @@
+package com.example.thagadur.android_session11_assignment2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+
+        import java.util.List;
+
+        import android.os.Bundle;
+        import android.app.Activity;
+        import android.widget.ArrayAdapter;
+
+public class MainActivity extends AppCompatActivity {
+
+    /*
+     * Change to type CustomAutoCompleteView instead of AutoCompleteTextView
+     * since we are extending to customize the view and disable filter
+     * The same with the XML view, type will be CustomAutoCompleteView
+     */
+    CustomAutoCompleteView myAutoComplete;
+
+    // adapter for auto-complete
+    ArrayAdapter<String> myAdapter;
+
+    // for database operations
+    DatabaseHandler databaseH;
+
+    // just to add some initial value
+    String[] item = new String[] {"Please search..."};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        try{
+
+            // instantiate database handler
+            databaseH = new DatabaseHandler(MainActivity.this);
+
+            // put sample data to database
+            insertSampleData();
+
+            // autocompletetextview is in activity_main.xml
+            myAutoComplete = (CustomAutoCompleteView) findViewById(R.id.myautocomplete);
+
+            // add the listener so it will tries to suggest while the user types
+            myAutoComplete.addTextChangedListener(new CustomAutoCompleteTextChangedListener(this));
+
+            // set our adapter
+            myAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, item);
+            myAutoComplete.setAdapter(myAdapter);
+
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void insertSampleData(){
+
+        // CREATE
+        databaseH.create( new MyObject("January") );
+        databaseH.create( new MyObject("February") );
+        databaseH.create( new MyObject("March") );
+        databaseH.create( new MyObject("April") );
+        databaseH.create( new MyObject("May") );
+        databaseH.create( new MyObject("June") );
+        databaseH.create( new MyObject("July") );
+        databaseH.create( new MyObject("August") );
+        databaseH.create( new MyObject("September") );
+        databaseH.create( new MyObject("October") );
+        databaseH.create( new MyObject("November") );
+        databaseH.create( new MyObject("December") );
+
+    }
+
+    // this function is used in CustomAutoCompleteTextChangedListener.java
+    public String[] getItemsFromDb(String searchTerm){
+
+        // add items on the array dynamically
+        List<MyObject> products = databaseH.read(searchTerm);
+        int rowCount = products.size();
+
+        String[] item = new String[rowCount];
+        int x = 0;
+
+        for (MyObject record : products) {
+
+            item[x] = record.objectName;
+            x++;
+        }
+
+        return item;
+    }
+
+}
---
+++ app/src/main/java/com/example/thagadur/android_session11_assignment2/MyObject.java
@@ -0,0 +1,16 @@
+package com.example.thagadur.android_session11_assignment2;
+
+/**
+ * Created by Thagadur on 9/25/2017.
+ */
+public class MyObject {
+
+    public String objectName;
+
+    // constructor for adding sample data
+    public MyObject(String objectName){
+
+        this.objectName = objectName;
+    }
+
+}
